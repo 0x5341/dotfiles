4fabf1ca22a8a633eacaf2a0c0d8f453c50e45d0
0x5341 <107183580+0x5341@users.noreply.github.com>
2025-12-31 20:29:01 +0900

    ...?

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 # install mise
 curl https://mise.run | sh
 
-# setup dotfiles
+# setup dotfiles (first)
 mise exec chezmoi -- chezmoi init --apply --verbose https://github.com/0x5341/dotfiles.git
+
+# install tools
+mise i
 
 # source .bashrc
 . ~/.bashrc
